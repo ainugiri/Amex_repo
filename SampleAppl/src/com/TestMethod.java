3895949089533f7ee10363561f5066da12bdec84
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-05-31 09:25:25 +0530

    Updated with age

--- SampleAppl/src/com/TestMethod.java
+++ SampleAppl/src/com/TestMethod.java
@@ -4,11 +4,11 @@ public class TestMethod {
 	int a;
 	public void testVal(int a) {
 		a = this.a;
-		if(a<10) {
-			System.out.println("Incorrect data");
+		if(a<18) {
+			System.out.println("Not a valid age");
 		}
 		else {
-			System.out.println("Valid Data");
+			System.out.println("Valid age");
 		}
 	}
 }
